83f3922f1a533290a8dec7c16cc6a62b142b47cd
Roohia-Bashir <160015350+Roohia-Bashir@users.noreply.github.com>
2024-03-12 07:56:02 -0700

    Update app.ts

--- 2.personal_message/app.ts
+++ 2.personal_message/app.ts
@@ -1,8 +1,8 @@
 let personName :string = '';
 
 if (personName !== null && personName !== ''){
-    cons(`Hello ${personName}, would you like to learn some python today?`)
+    console.log(`Hello ${personName}, would you like to learn some python today?`)
 }
 else {
-    cons(`you have to fill your name !`)
+    console.log(`you have to fill your name !`)
 }
