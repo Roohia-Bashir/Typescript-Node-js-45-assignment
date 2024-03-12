c8f5e12d24ad73ae110a57966f6feb19ea72a4f0
Roohia-Bashir <160015350+Roohia-Bashir@users.noreply.github.com>
2024-03-12 07:55:32 -0700

    Update app.js

--- 2.personal_message/app.js
+++ 2.personal_message/app.js
@@ -1,9 +1,8 @@
 "use strict";
 let personName = '';
-personName = prompt('what is your name') || '';
 if (personName !== null && personName !== '') {
-    alert(`hello ${personName}, would you like to learn some python today`);
+    console.log(`hello ${personName}, would you like to learn some python today?`);
 }
 else {
-    alert(`you have to fill your name !`);
+    console.log(`you have to fill your name !`);
 }
