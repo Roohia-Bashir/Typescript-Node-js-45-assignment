42c5bdd954fe22b1fff0b6e4883f10cc6981a306
Roohia-Bashir <160015350+Roohia-Bashir@users.noreply.github.com>
2024-03-12 07:53:00 -0700

    Update app.ts

--- 2.personal_message/app.ts
+++ 2.personal_message/app.ts
@@ -1,10 +1,8 @@
 let personName :string = '';
 
-personName = prompt('what is your name') || '';
-
 if (personName !== null && personName !== ''){
-    alert (`hello ${personName}, would you like to learn some python today`)
+    cons(`Hello ${personName}, would you like to learn some python today?`)
 }
 else {
-    alert (`you have to fill your name !`)
+    cons(`you have to fill your name !`)
 }
